b3374dcc3a6a0a291262a820f60149cba5bdafb6
Vikram <vikramgithub3006@gmail.com>
2026-06-30 12:11:56 +0530

    Create Calculator of +,-,* using def function.py

---
+++ Calculator of +,-,* using def function.py
@@ -0,0 +1,13 @@
+class calculator:
+  def add(self,a,b):
+    return a+b
+  def sub(self,a,b):
+    return a-b
+  def mul(self,a,b):
+    return a*b
+  def div(self,a,b):
+    return a/b
+print(calculator().add(2,3))
+print(calculator().sub(4,3))
+print(calculator().mul(2,3))
+print(calculator().div(6,3))
