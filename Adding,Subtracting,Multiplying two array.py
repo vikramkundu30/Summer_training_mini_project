b045960371633f434b03acf593bb145099cfe7c9
Vikram <vikramgithub3006@gmail.com>
2026-06-30 12:26:09 +0530

    Create Adding,Subtracting,Multiplying two array.py

---
+++ Adding,Subtracting,Multiplying two array.py
@@ -0,0 +1,14 @@
+import numpy as np
+array1 = np.array([10, 20, 30, 40])
+array2 = np.array([2, 4, 6, 8])
+print("Initial Arrays:")
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("-" * 30)
+add_operator = array1 + array2
+print("Addition (+):      ", add_operator)
+sub_operator = array1 - array2
+print("Subtraction (-):   ", sub_operator)
+mul_operator = array1 * array2
+print("Multiplication (*):", mul_operator)
+print("-" * 30)
